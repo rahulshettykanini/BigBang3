2b9fcc968a849018f5522b7b2b4a3cf13847be5c
rahulshettykanini <127296914+rahulshettykanini@users.noreply.github.com>
2023-08-07 14:30:09 +0530

    Create AgentPackages.js

---
+++ Frontend/src/Components/AgentPackages.js
@@ -0,0 +1,164 @@
+import { Container, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+import React, {  useState } from 'react';
+
+function AgentPackages({id}) {
+
+
+    const [ac,setac] = useState([]);
+
+    const [bc,setbc] = useState([]);
+
+    const [cc,setcc] = useState([]);
+
+    const AgentPackagess = async ()=>
+{
+  await fetch('https://localhost:7112/api/Packages/' + id + 'agepac').then(response => response.json()).then(data =>setac(data))
+
+}
+
+const AgentBookings = async ()=>
+{
+  await fetch('https://localhost:7112/api/BookingTrips/' + id + 'agent').then(response => response.json()).then(data =>setbc(data))
+
+}
+
+const AgentHotels = async ()=>
+{
+  await fetch('https://localhost:7112/api/Hotels/' + id + 'agenthotel').then(response => response.json()).then(data =>setcc(data))
+
+}
+
+
+  return (
+    <div>
+<div>
+
+    <div>
+      <button onClick={AgentPackagess}>SEE AGENT PACKAGES</button>
+      </div>
+    
+    
+   
+
+    <Container>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>PACKAGE ID</TableCell>
+              <TableCell>PACKAGE NAME</TableCell>
+              <TableCell>PACKAGE PLACE</TableCell>
+              <TableCell>PACKAGE DURATION</TableCell>
+              <TableCell>PACKAGE PRICE</TableCell>
+              <TableCell>PACKAGE DESCRIPTION</TableCell>
+              {/* Add more table headers if needed */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ac.map((act) => (
+              <TableRow key={act.packageId}>
+                <TableCell>{act.packageId}</TableCell>
+                <TableCell>{act.packageName}</TableCell>
+                <TableCell>{act.place}</TableCell>
+                <TableCell>{act.duration}</TableCell>
+                <TableCell>{act.packagePrice}</TableCell>
+                <TableCell>{act.description}</TableCell>
+                {/* Add more table cells with corresponding data */}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+    </div>
+
+    <div>
+
+<div>
+  <button onClick={AgentBookings}>SEE MY BOOKINGS</button>
+  </div>
+
+
+
+
+<Container>
+  <TableContainer>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell> NAME</TableCell>
+          <TableCell>EMAIL ID</TableCell>
+          <TableCell>ADDRESS</TableCell>
+          <TableCell>PACKAGE NAME</TableCell>
+          <TableCell>HOTEL NAME</TableCell>
+          <TableCell>TOUR DATE</TableCell>
+          <TableCell>BOOKING DATE</TableCell>
+          <TableCell>NUMBER OF TOURISTS</TableCell>
+          <TableCell>TOTAL AMOUNT</TableCell>
+          {/* Add more table headers if needed */}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {bc.map((bct) => (
+          <TableRow key={bct.name}>
+            <TableCell>{bct.name}</TableCell>
+            <TableCell>{bct.email}</TableCell>
+            <TableCell>{bct.address}</TableCell>
+            <TableCell>{bct.packageName}</TableCell>
+            <TableCell>{bct.hotelname}</TableCell>
+            <TableCell>{bct.dateOfTheTrip}</TableCell>
+            <TableCell>{bct.dateofBooking}</TableCell>
+            <TableCell>{bct.numberOfPeople}</TableCell>
+            <TableCell>{bct.totalAmount}</TableCell>
+            {/* Add more table cells with corresponding data */}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+</Container>
+</div>
+
+<div>
+
+<div>
+  <button onClick={AgentHotels}>SEE MY HOTEL</button>
+  </div>
+
+
+
+
+<Container>
+  <TableContainer>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell> HOTEL NAME</TableCell>
+          <TableCell>PACKAGE ID</TableCell>
+          <TableCell>HOTEL RATING</TableCell>
+          <TableCell>HOTEL PRICE</TableCell>
+          {/* Add more table headers if needed */}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {cc.map((cct) => (
+          <TableRow key={cct.hotelid}>
+            <TableCell>{cct.hotelname}</TableCell>
+            <TableCell>{cct.packageId}</TableCell>
+            <TableCell>{cct.hotelRating}</TableCell>
+            <TableCell>{cct.hotelPrice}</TableCell>
+            {/* Add more table cells with corresponding data */}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+</Container>
+</div>
+
+</div>
+  );
+}
+
+export default AgentPackages;
